6eebf2d3b75a918b998e18ace57b74e38af63f15
chyzwar <schizek.marcin@gmail.com>
2024-08-20 12:50:37 +0200

    feat(): added examples support

--- packages/configurator/src/components/MapboxSplitPanelExample.tsx
+++ packages/configurator/src/components/MapboxSplitPanelExample.tsx
@@ -1,26 +1,40 @@
 import { type MapboxMapInstance, MapboxMap } from "@mapka/mapbox-map";
-import { useMemo, useState } from "react";
+import { type FC, useMemo, useState } from "react";
 import { CodeEditor } from "./CodeEditor";
+import type { MapExample } from "./SplitPanelExample";
 
 const accessToken =
   "pk.eyJ1IjoibWFyY2lua29wYWN6IiwiYSI6ImNrenlteHJvaTAxdWUzY254ZHppMG5nN3QifQ.U3tuBCRNFosiS3buKpUxnQ";
 
-export const MapboxSplitPanelExample = () => {
-  const [mapMapbox, setMapMapbox] = useState<MapboxMapInstance | null>(null);
-  const contextMapMapbox = useMemo(() => {
+export interface MapboxSplitPanelExampleProps {
+  example?: MapExample;
+}
+
+export const MapboxSplitPanelExample: FC<MapboxSplitPanelExampleProps> = (props) => {
+  const {
+    example: { code } = {},
+  } = props;
+
+  const [map, setMap] = useState<MapboxMapInstance | null>(null);
+  const context = useMemo(() => {
+    if (!map) return;
     return {
-      map: mapMapbox,
+      map,
     };
-  }, [mapMapbox]);
+  }, [map]);
 
   return (
     <split-panel>
       <div slot="map-panel">
-        <MapboxMap onMapLoaded={setMapMapbox} accessToken={accessToken} />
+        <MapboxMap onMapLoaded={setMap} accessToken={accessToken} />
       </div>
 
       <div slot="content-panel">
-        <CodeEditor context={contextMapMapbox} />
+        <CodeEditor 
+          context={context} 
+          defaultValue={code} 
+          waitForContext 
+        />
       </div>
     </split-panel>
   );
---
+++ packages/configurator/src/utils/dash.ts
@@ -0,0 +1,7 @@
+
+export function trimCode(numSpaces: number, code: string) {
+  return code
+    .split("\n")
+    .map((line) => line.slice(numSpaces))
+    .join("\n");
+}
