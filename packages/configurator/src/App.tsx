439b338d3ef313a9ab488458cd014df56e32dd35
chyzwar <marcin@mapka.dev>
2025-04-28 09:58:30 +0200

    feat(): basic maps implemented

--- packages/configurator/src/App.tsx
+++ packages/configurator/src/App.tsx
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import { MapboxMap } from "@mapka/mapbox-map/src";
+import { MapLibreMap } from "@mapka/maplibre-map";
 
 function App() {
-  return <div />;
+  return (
+    <>
+      <MapLibreMap height={400} width={400}/>
+      <MapboxMap 
+        height={400} 
+        width={400}
+        accessToken="pk.eyJ1IjoibWFyY2lua29wYWN6IiwiYSI6ImNrenlteHJvaTAxdWUzY254ZHppMG5nN3QifQ.U3tuBCRNFosiS3buKpUxnQ"
+      />
+    </>
+);
 }
 
 export default App;
--- packages/map/src/Map.ts
+++
@@ -1,3 +0,0 @@
-class MapkaMap extends HTMLElement {}
-
-customElements.define("mapka-map", MapkaMap);
